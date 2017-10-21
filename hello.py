4696c3e62fa5927334c5a8b350a0def9beda022d
mercuryZ <15210820708@163.com>
2017-10-21 12:57:13 +0800

    templates

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
 
 # 1.程序初始化
@@ -10,11 +10,11 @@ manager = Manager(app)
 # 2.写路由 视图函数
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 # 3.启动服务器
 if __name__ == '__main__':
