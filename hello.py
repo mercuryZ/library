05f0e609d8032644635d78d4b0b556929483d56e
mercuryZ <15210820708@163.com>
2017-10-22 12:07:23 +0800

    form first time

--- hello.py
+++ hello.py
@@ -3,16 +3,30 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 # 1.程序初始化
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 # 2.写路由 视图函数
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
